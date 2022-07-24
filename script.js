8257c6a269fb12466e5a8324f12724b87dff6709
jonathan010603 <jonathan010603@gmail.com>
2022-07-24 20:26:10 -0300

    build (Finished the basic project)!: finished
    Fundamental features working.
    I'll be thinking about adding some animations.

--- script.js
+++ script.js
@@ -1,23 +1,49 @@
+let page = 0;
+let imageDiv = document.querySelector(".image");
 let title = document.querySelector(".title");
 let subtitle = document.querySelector(".subtitle");
-let page = 0;
+let nextButton = document.querySelector(".next");
+let skipButton = document.querySelector('.skip');
 
 const data = [
     {
         title: "<b>Plant lover Community</b>",
-        subtitle: "Find gardening enthusiasts from all over the world"
+        subtitle: "Find gardening enthusiasts from all over the world",
+        button: "Continue"
     },
 
     {
         title: "<b>Get fast & safe delivery</b>",
-        subtitle: "Get good quality products for your plants"
+        subtitle: "Get good quality products for your plants",
+        button: "Continue"
     },
 
     {
         title: "<b>Buy & Sell Tools</b>",
-        subtitle: "Buy & sell good quality products for your beautiful plants"
+        subtitle: "Buy & sell good quality products for your beautiful plants",
+        button: "Get started"
     },
 ]
 
-title.innerHTML = data[page].title;
-subtitle.innerHTML = data[page].subtitle;
+const setPage = () => {
+    document.querySelector(`.point:nth-child(${page + 1})`).classList.add('selected');
+    imageDiv.classList.add(`bg${page}`);
+    title.innerHTML = data[page].title;
+    subtitle.innerHTML = data[page].subtitle;
+    nextButton.innerHTML = data[page].button;
+    
+    page === 2 
+    ? skipButton.classList.add('hide')
+    : skipButton.classList.remove('hide')
+}
+
+const nextPage = (i) => {
+    document.querySelector(`.point:nth-child(${page + 1})`).classList.remove('selected');
+    imageDiv.classList.remove(`bg${page}`);
+    !isNaN(i) 
+    ? ( page = i )
+    : ( page === 2 ? ( page = 0 ) : ( page += 1 ) )
+    setPage();
+}
+
+setPage();
